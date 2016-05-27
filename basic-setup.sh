789bf172bf7e27b76a441c672f96e8209ed1e0d8
username <username@example.com>
2016-05-27 11:21:12 +0800

    basic conf setup

--- basic-setup.sh
+++ basic-setup.sh
@@ -1,5 +1,6 @@
-ln -s utility-config/.git
+#ln -s utility-config/.git
 ln -s utility-config/.gitconfig .gitconfig
 ln -s utility-config/.tmux.conf .tmux.conf
 ln -s utility-config/.vimrc .vimrc
+ln -s utility-config/.zshrc .zshrc
 

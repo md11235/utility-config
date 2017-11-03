e0188d68c333e850cd8f643a55162e70e2212014
Madsen Zhang <md11235@gmail.com>
2017-11-03 17:24:39 +0300

    switch from wget to curl
    It seems macOS has not wget by default. Let's see...

--- install_env.sh
+++ install_env.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-cd ~/
-wget https://raw.githubusercontent.com/md11235/utility-config/master/.zshrc
-wget https://raw.githubusercontent.com/md11235/utility-config/master/.tmux.conf
+#cd ~/
+#wget https://raw.githubusercontent.com/md11235/utility-config/master/.zshrc
+#wget https://raw.githubusercontent.com/md11235/utility-config/master/.tmux.conf
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+echo "$(curl -fsSL https://raw.githubusercontent.com/md11235/utility-config/master/.tmux.conf)" > .tmux.conf

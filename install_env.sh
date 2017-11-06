196df9f9d7c5903b16b863bc10e180a5fefb6b66
Madsen Zhang <sen.zhang@126.com>
2017-11-06 16:49:01 +0800

    试图修改zsh默认主题为candy

--- install_env.sh
+++ install_env.sh
@@ -4,5 +4,6 @@
 #wget https://raw.githubusercontent.com/md11235/utility-config/master/.zshrc
 #wget https://raw.githubusercontent.com/md11235/utility-config/master/.tmux.conf
 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 echo "$(curl -fsSL https://raw.githubusercontent.com/md11235/utility-config/master/.tmux.conf)" > .tmux.conf
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+sh -c "sed -i 's/ZSH_THEME=\"robbyrussell\"/ZSH_THEME=\"candy\"/'"

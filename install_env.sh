3d18c4010f940e48f18a1a53acbdd47f063302c8
Madsen Zhang <sen.zhang@126.com>
2017-11-06 16:53:14 +0800

    啊，忘了给.zshrc文件

--- install_env.sh
+++ install_env.sh
@@ -6,4 +6,4 @@
 
 echo "$(curl -fsSL https://raw.githubusercontent.com/md11235/utility-config/master/.tmux.conf)" > .tmux.conf
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-sh -c "sed -i 's/ZSH_THEME=\"robbyrussell\"/ZSH_THEME=\"candy\"/'"
+sh -c "sed -i 's/ZSH_THEME=\"robbyrussell\"/ZSH_THEME=\"candy\"/' .zshrc"

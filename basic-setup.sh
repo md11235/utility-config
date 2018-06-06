7cceaa50897802cbbb54fd90bb7c8fe811f1523d
Madsen <md11235@gmail.com>
2018-06-06 16:47:36 +0800

    add git global attributes for juypter

--- basic-setup.sh
+++ basic-setup.sh
@@ -2,4 +2,6 @@ ln -sf utility-config/.zshrc .zshrc
 ln -sf utility-config/.gitconfig .gitconfig
 ln -sf utility-config/.tmux.conf .tmux.conf
 ln -sf utility-config/.vimrc .vimrc
+ln -sf utility-config/.gitattributes_global 
+
 
